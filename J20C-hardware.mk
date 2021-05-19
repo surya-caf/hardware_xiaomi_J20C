63fbc5e95fac1917c2aee27cda1340efe9b41176
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-05-19 18:42:06 +0300

    configs: import aosp audio_policy_volumes.xml
    As of 24b0a89 - "Hearing Aid volume curve"
    
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>

--- J20C-hardware.mk
+++ J20C-hardware.mk
@@ -15,3 +15,4 @@ PRODUCT_COPY_FILES += \
     hardware/xiaomi/J20C/configs/qdcm_calib_data_nt36672c_huaxing_fhd_video_mode_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_nt36672c_huaxing_fhd_video_mode_dsi_panel.xml \
     hardware/xiaomi/J20C/configs/qdcm_calib_data_nt36672c_tianma_fhd_video_mode_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_nt36672c_tianma_fhd_video_mode_dsi_panel.xml \
     hardware/xiaomi/J20C/configs/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    hardware/xiaomi/J20C/configs/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
